2306dbc8d7258147190aab09cf6109d10e3506bf
cmh <1325573969@qq.com>
2023-04-16 17:02:53 +0800

    [minghao chen] mybatis案例

--- maven-demo/src/main/java/com/cmh/Main.java
+++ maven-demo/src/main/java/com/cmh/Main.java
@@ -1,3 +1,5 @@
+package com.cmh;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ mybatis-demo/src/test/java/com/itheima/test/MyBatis.java
@@ -0,0 +1,31 @@
+package com.itheima.test;
+
+import com.itheima.mapper.BrandMapper;
+import com.itheima.pojo.Brand;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MyBatis {
+    @Test
+    public  void test() throws IOException {
+        //1. 加载mybatis的核心配置文件，获取 SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+        //获取sqlsession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        List<Brand> brands = brandMapper.selectAll();
+        System.out.println(brands);
+    }
+}
